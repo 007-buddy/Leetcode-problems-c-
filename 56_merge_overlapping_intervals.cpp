06b0bfaf7d8f445bb20907a41c864afeaf9ab667
Vinay <vinaypratap0455@gmail.com>
2025-12-22 03:15:03 +0530

    Add implementation for merging overlapping intervals

---
+++ 56_merge_overlapping_intervals.cpp
@@ -0,0 +1,27 @@
+// uses a result vector to store merged intervals
+// time complexity : O(NlogN) and space complexity : O(N)
+
+#include<bits/stdc++.h>
+using namespace std;    
+class Solution {
+  public:
+    vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
+        sort(arr.begin(),arr.end());
+        vector<vector<int>>result;
+        result.push_back(arr[0]);
+        int n=arr.size();
+        for(int i=1;i<n;i++){
+           int start=arr[i][0];
+           int end=arr[i][1];
+            if(result.back()[1]>=start){
+                result.back()[1]=max(result.back()[1],end);
+            }
+            else{
+                result.push_back(arr[i]);
+            }
+            
+        }
+        return result;
+        
+    }
+};
