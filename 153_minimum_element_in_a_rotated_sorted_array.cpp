60f5592809ede32b29bf44fe79279ffcb42dd0c1
Vinay <vinaypratap0455@gmail.com>
2025-12-22 03:27:47 +0530

    Add implementation for finding the minimum element in a rotated sorted array

---
+++ 153_minimum_element_in_a_rotated_sorted_array.cpp
@@ -0,0 +1,23 @@
+// uses binary search to find the minimum element in a rotated sorted array
+// time complexity : O(logN) and space complexity : O(1)
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int findMin(vector<int>& arr) {
+        int low=0;
+        int high =arr.size()-1;
+        while(low<high){
+            int mid=(low+high)/2;
+            if(arr[mid]>arr[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        // *min_element(arr.begin(),arr.end());} built in function to find the minimum element in the array.
+     return arr[low];
+    }
+};
