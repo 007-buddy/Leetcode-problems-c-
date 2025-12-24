7b7268c9634dd557fd27fce1fd3a17ebe7458d79
Vinay <vinaypratap0455@gmail.com>
2025-12-24 14:09:16 +0530

    Add implementation for counting subarrays that sum to a given value k using cumulative sums

---
+++ 560_subarray_sum_equal_k.cpp
@@ -0,0 +1,21 @@
+// implementingng a function to find the number of subarrays that sum to a given value k
+// the below function uses an unordered_map to store the cumulative sums and their frequencies
+
+// time complexity: O(n) and space complexity: O(n)
+
+#include <bits/stdc++.h>
+using namespace std;
+long countNumberOfSubarrays(vector<int> arr, int k) {
+    long long total=0;
+    long count=0;
+    unordered_map<int,int>subnum;
+    subnum[0]=1;
+    for(int i:arr){
+    total+=i;
+    if(subnum.find(total-k)!=subnum.end()){
+        count+=subnum[total-k];
+    }
+    subnum[total]++;
+}
+return count;
+}
