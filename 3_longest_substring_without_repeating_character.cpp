10e1c3622d2b178f099db5108984e1f0944c7d4f
Vinay <vinaypratap0455@gmail.com>
2025-12-24 21:03:24 +0530

    Add implementation for finding the length of the longest substring without repeating characters using a sliding window technique

---
+++ 3_longest_substring_without_repeating_character.cpp
@@ -0,0 +1,22 @@
+// uses a sliding window technique  and a set to keep track of characters in the current window
+// time complexity: O(n) and space complexity: O(min(m,n)) where m is the size of the character set
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        set<char>ch;
+     int len=0, l=0;
+ for(int i=0;i<s.size();i++){
+    while(ch.find(s[i])!=ch.end()){
+            ch.erase(s[l]);
+            l++;
+            }
+        ch.insert(s[i]);
+        len=max(len,i-l+1);
+        }
+     return len;
+    }
+};
