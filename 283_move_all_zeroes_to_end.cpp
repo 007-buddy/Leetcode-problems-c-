80d9ef7a04c4cb145c32f60fdb86017f0cbb6a92
Vinay <vinaypratap0455@gmail.com>
2025-12-24 20:32:32 +0530

    Add implementation for moving all zeroes to the end of an array .

---
+++ 283_move_all_zeroes_to_end.cpp
@@ -0,0 +1,26 @@
+/*uses two-pointer technique to move all zeroes in the array
+ to the end while maintaining the order of non-zero elements
+ */
+
+ // uses two loops first will move all the non-zero elements to the front and the last one will fill the rest of the array with zeroes
+ // time complexity: O(n) and space complexity: O(1)
+#include <bits/stdc++.h>
+using namespace std;
+
+void pushZerosToEnd(vector<int> &arr)
+{
+    int i = 0;
+    for (int j = 0; j < arr.size(); j++)
+    {
+        if (arr[j] != 0)
+        {
+            arr[i] = arr[j];
+            i++;
+        }
+    }
+    while (i < arr.size())
+    {
+        arr[i] = 0;
+        i++;
+    }
+}
